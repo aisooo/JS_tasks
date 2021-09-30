d1694d5e4a1450d15a7608728c6483f1beaee444
aisooo <72738840+aisooo@users.noreply.github.com>
2021-09-30 14:16:42 +0600

    added problems

--- javascript/tasks3.js
+++ javascript/tasks3.js
@@ -10,10 +10,10 @@ btn_text.onclick = function () {
 };
 //_________ВОПРОС№4.3_________
 
-var list = document.getElementById("todo-ol");
 var createList = document.getElementById("ToDoid");
 
-btnCreate.onclick = function () {
+createList.onclick = function () {
+  var list = document.getElementById("todo-ol");
   var listOfitems = [
     "Buy lemonade",
     "Make toasts",
@@ -23,9 +23,36 @@ btnCreate.onclick = function () {
   ];
 
   for (var i = 0; i < listOfitems.length; i++) {
-    var listOfitems = listOfitems[i];
+    var li = listOfitems[i];
     var li = document.createElement("li");
-    li.innerHTML = listOfitems;
-    ol.append(li);
+    li.className = "question-item";
+    li.innerHTML = listOfitems[i];
+    list.appendChild(li);
   }
 };
+var p_element = document.querySelectorAll(".set-hr");
+var button_hr = document.getElementById("btn-hr");
+
+button_hr.onclick = function () {
+  var hr = createElement("hr");
+  for (let i = 0; i < p_element.length; i++) {
+    let ls = p_element[i];
+    hr.appendChild(ls);
+    console.log(hr);
+  }
+};
+
+//_________ВОПРОС№5_________
+var lastQua = document.querySelectorAll(".item");
+var lastQua2 = document.getElementById("last_btns");
+
+lastQua2.addEventListener("click", (e) => {
+  lastQua.forEach((item) => {
+    if (item.innerHTML === "Cola 1.5 l. x 1") {
+      item.remove();
+    }
+    if (item.innerHTML === "Chocolate bar x 3") {
+      item.innerHTML = "Canned Fish x 4";
+    }
+  });
+});
